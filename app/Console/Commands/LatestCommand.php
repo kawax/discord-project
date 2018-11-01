fc5234be28a25ce9e0d0f346551e0432e95155bf
kawax <kawaxbiz@gmail.com>
2018-11-01 20:02:32 +0900

    latest

---
+++ app/Console/Commands/LatestCommand.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use GrahamCampbell\GitHub\Facades\GitHub;
+use RestCord\DiscordClient;
+
+class LatestCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'discord:latest';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Latest';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @param DiscordClient $client
+     *
+     * @return mixed
+     */
+    public function handle(DiscordClient $client)
+    {
+        $release = Github::repo()->releases()->latest('laravel', 'framework');
+
+        $ver = $release['tag_name'];
+
+        if ($ver === cache('latest_ver')) {
+            return;
+        }
+
+        cache()->forever('latest_ver', $ver);
+
+        try {
+            $client->channel->createMessage([
+                'channel.id' => (int)config('services.discord.laravel_channel'),
+                'content'    => $release['html_url'],
+                'embed'      => [
+                    //                    "title"       => $release['tag_name'],
+                    //                    "url"         => $release['html_url'],
+                    "description" => str_limit($release['body'], 2000),
+                ],
+            ]);
+
+        } catch (\Exception $e) {
+            logger()->error($e->getMessage());
+        }
+    }
+}
--- config/services.php
+++ config/services.php
@@ -41,13 +41,14 @@ return [
     ],
 
     'discord' => [
-        'token'   => env('DISCORD_BOT_TOKEN'),
-        'channel' => env('DISCORD_CHANNEL'),
-        'guild'   => (int)env('DISCORD_GUILD'),
-        'bot'     => (int)env('DISCORD_BOT_ID'),
-        'admin'   => (int)env('DISCORD_ADMIN_ID'),
-        'private' => (int)env('DISCORD_PRIVATE'),
-        'role'    => (int)env('DISCORD_ROLE'),
+        'token'           => env('DISCORD_BOT_TOKEN'),
+        'channel'         => env('DISCORD_CHANNEL'),
+        'guild'           => (int)env('DISCORD_GUILD'),
+        'bot'             => (int)env('DISCORD_BOT_ID'),
+        'admin'           => (int)env('DISCORD_ADMIN_ID'),
+        'private'         => (int)env('DISCORD_PRIVATE'),
+        'role'            => (int)env('DISCORD_ROLE'),
+        'laravel_channel' => env('DISCORD_LARAVEL_CHANNEL'),
     ],
 
 ];
