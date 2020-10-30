decd00c6bd0ae048a8246983b8bc13cd491a9490
kawax <kawaxbiz@gmail.com>
2020-10-31 00:16:26 +0900

    wip

--- app/Console/Commands/ServeCommand.php
+++ app/Console/Commands/ServeCommand.php
@@ -62,7 +62,7 @@ class ServeCommand extends Command
             'message',
             function (Message $message) {
                 //dd($message->channel);
-                echo 'Received Message from '.$message->author->tag.' in '.($message->channel instanceof DMChannelInterface ? 'DM' : 'channel #'.$message->channel->name).' with '.$message->attachments->count(
+                echo 'Received Message from '.$message->author->tag.' in '.($message->channel instanceof DMChannelInterface ? 'DM' : 'channel #'.$message->channel->name ?? 'channel').' with '.$message->attachments->count(
                     ).' attachment(s) and '.\count($message->embeds).' embed(s)'.PHP_EOL;
 
                 if ($message->author->bot) {
